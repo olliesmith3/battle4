63efa15d0130bd1c442e36aad535c83216152930
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-15 17:08:13 +0100

    added a get /play route, created a session hash, and enabled sessions.

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require "sinatra"
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get "/" do
     erb :index
   end
 
-  post '/names' do
+  post "/names" do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect "/play"
+  end
 
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+  get "/play" do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
