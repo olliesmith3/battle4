c0037f9ffffd7dfc9122350c2f48972393189b4e
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-16 14:02:38 +0100

    commiting before pull

--- spec/features/get_names_spec.rb
+++ spec/features/get_names_spec.rb
@@ -1,9 +1,9 @@
-feature 'Enter 2 player names ' do
+feature "Enter 2 player names " do
   scenario "players enter name into form" do
-    visit('/')
+    visit("/")
     fill_in :"player_1_name", with: "Charlie"
     fill_in :"player_2_name", with: "Gary"
-    click_button 'Submit'
-    expect(page).to have_content 'Charlie vs. Gary'
+    click_button "Submit"
+    expect(page).to have_content "Charlie vs. Gary"
   end
 end
